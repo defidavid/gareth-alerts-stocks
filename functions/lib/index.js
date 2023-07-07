fc5e2eec7fee4d244dc63ef2ec4c9d807e467f12
defidavid <david@hifi.finance>
2023-07-06 20:29:24 -0500

    Remove lib

--- functions/lib/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const processZapierEmailWebhook_1 = require("./modules/processZapierEmailWebhook");
-exports.processZapierEmailWebhook = processZapierEmailWebhook_1.processZapierEmailWebhook;
-//# sourceMappingURL=index.js.map
