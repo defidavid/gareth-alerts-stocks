d55b0068bdb55a84ec789707a2686566613e7dcc
defidavid <david@hifi.finance>
2023-10-13 13:01:39 -0500

    remove exit black list

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -3,4 +3,4 @@ export const THEORETICAL_PORTFOLIO_SIZE = 1_000_000;
 export const MAX_PURCHASE_AMOUNT = THEORETICAL_PORTFOLIO_SIZE * 0.02;
 export const PRICE_WIGGLE_ROOM_PERCENT = 0.0075;
 export const IS_PAPER_TRADING = false;
-export const EXIT_BLACK_LIST = [];
+export const EXIT_BLACK_LIST: string[] = [];
